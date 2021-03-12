c0e366e31a7b9fe7697a7b47d8b12af726d50a56
Ranko Liang <rankoliang@gmail.com>
2021-03-12 16:12:36 -0500

    Add GameNavbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import GameNavbar from './components/GameNavbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GameNavbar />
     </div>
   );
 }
---
+++ src/components/GameNavbar.jsx
@@ -0,0 +1,21 @@
+import { Navbar } from 'react-bulma-components';
+
+const GameNavbar = () => {
+  return (
+    <Navbar
+      color="light"
+      role="navigation"
+      aria-label="main navigation"
+      fixed="top"
+    >
+      <Navbar.Brand>
+        <Navbar.Item renderAs="div">Waldo</Navbar.Item>
+        <Navbar.Item renderAs="div">Wizard</Navbar.Item>
+        <Navbar.Item renderAs="div">Wilma</Navbar.Item>
+        <Navbar.Item>Leaderboard</Navbar.Item>
+      </Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default GameNavbar;

385ebe6b69e0cd4a9862c532ebb2b6efd34b38d5
Ranko Liang <rankoliang@gmail.com>
2021-03-12 22:01:30 -0500

    Add Waldo image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import GameNavbar from './components/GameNavbar';
+import waldoPicture from './images/Waldo.jpg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <GameNavbar />
+      <ScrollableImage
+        className="waldo-picture"
+        src={waldoPicture}
+        alt="Find Waldo!"
+      />
     </div>
   );
 }
+
+const ScrollableImage = ({ src, alt, ...props }) => {
+  return (
+    <div className="scrollable-wrapper">
+      <img src={src} alt={alt} {...props} />
+    </div>
+  );
+};
 
 export default App;

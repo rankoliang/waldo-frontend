0d3fc18a20c98c2dd2a483d4aad6c1cfff2e4714
Ranko Liang <rankoliang@gmail.com>
2021-03-13 00:32:13 -0500

    Add image scrollbars

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import GameNavbar from './components/GameNavbar';
 import waldoPicture from './images/Waldo.jpg';
@@ -8,18 +9,37 @@ function App() {
     <div className="App">
       <GameNavbar />
       <ScrollableImage
-        className="waldo-picture"
         src={waldoPicture}
         alt="Find Waldo!"
+        zoom="0.6"
       />
     </div>
   );
 }
 
-const ScrollableImage = ({ src, alt, ismap, ...props }) => {
+const ScrollableImage = ({ src, alt, zoom, ...props }) => {
+  const [dimensions, setDimensions] = useState({ height: 0, width: 0 });
+  const { height, width } = dimensions;
+
   return (
     <div className="scrollable-wrapper">
-      <img src={src} alt={alt} ismap={ismap} {...props} />
+      <img
+        style={{
+          minHeight: height,
+          minWidth: width,
+          verticalAlign: 'bottom',
+          zoom,
+        }}
+        src={src}
+        alt={alt}
+        onLoad={({ target }) => {
+          setDimensions({
+            height: target.naturalHeight,
+            width: target.naturalWidth,
+          });
+        }}
+        {...props}
+      />
     </div>
   );
 };

d60533073dff733a5b60c260172a04c1651f65bc
Ranko Liang <rankoliang@gmail.com>
2021-03-16 18:16:37 -0400

    Add active styling to navbar

--- src/components/GameNavbar.jsx
+++ src/components/GameNavbar.jsx
@@ -1,11 +1,16 @@
 import { Navbar } from 'react-bulma-components';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const GameNavbar = () => {
   return (
     <Navbar color="dark" role="navigation" aria-label="main navigation">
       <Navbar.Brand>
-        <Navbar.Item renderAs={Link} to="/">
+        <Navbar.Item
+          renderAs={NavLink}
+          exact
+          to="/"
+          activeClassName="is-active"
+        >
           Home
         </Navbar.Item>
         <Navbar.Item>Leaderboard</Navbar.Item>
